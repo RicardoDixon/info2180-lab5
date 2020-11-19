8270490d23c0f4e1d675898658122522b8de1d6f
Ricardo Dixon <rickydixxx@gmail.com>
2020-11-19 16:41:49 -0500

    completed lab5

--- world.js
+++ world.js
@@ -1,15 +1,23 @@
 window.onload=function(){
 
     let btn = document.getElementById("lookup");
-    
-  
+    let city = document.getElementById("lookup_cities");  
+    let cities = "";
     btn.addEventListener("click",function(element){
       element.preventDefault();
       
       loadDoc();
   
     });
-  
+
+    city.addEventListener("click",function(element){
+        element.preventDefault();
+        if(document.getElementById("country").value !=""){
+           cities = "cities" 
+        }
+        loadDoc();
+    
+      });
     function loadDoc() {
       var text = document.getElementById("country").value;
       var xhttp = new XMLHttpRequest();
@@ -19,7 +27,7 @@ window.onload=function(){
             document.getElementById("result").innerHTML = rest;
         }
       };
-      xhttp.open("GET", "world.php"+"?country="+text, true);
+      xhttp.open("GET", "world.php"+"?country="+text+"&context="+cities, true);
       xhttp.send();
     }
   }
--- world.php
+++ world.php
@@ -4,30 +4,55 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country=$_GET['country'];
+$cities=$_GET['context'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<table>
-  <tr> 
-    <th>Country Name</th>
-    <th>Continent</th>
-    <th>Independence Year</th>
-    <th>Head of State</th>
-  </tr>";
-  
-foreach ($results as $row){
-  echo "<tr>";
-  echo "<td>" . $row['name'] . "</td>";
-  echo "<td>" . $row['continent'] . "</td>";
-  echo "<td>" . $row['independence_year'] . "</td>";
-  echo "<td>" . $row['head_of_state'] . "</td>";
-  echo "</tr>";
+if(!empty($cities)){
+
+    $stmt = $conn->query("SELECT cities.district,cities.name,cities.population FROM cities JOIN countries ON countries.code=cities.country_code WHERE countries.name='$country'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table>
+    <tr> 
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
+    </tr>";
+    
+  foreach ($results as $row){
+    echo "<tr>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['district'] . "</td>";
+    echo "<td>" . $row['population'] . "</td>";
+    echo "</tr>";
   }
+
+}
+  if(empty($cities)){
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+      echo "<table>
+      <tr> 
+        <th>Country Name</th>
+        <th>Continent</th>
+        <th>Independence Year</th>
+        <th>Head of State</th>
+      </tr>";
+      
+    foreach ($results as $row){
+      echo "<tr>";
+      echo "<td>" . $row['name'] . "</td>";
+      echo "<td>" . $row['continent'] . "</td>";
+      echo "<td>" . $row['independence_year'] . "</td>";
+      echo "<td>" . $row['head_of_state'] . "</td>";
+      echo "</tr>";
+    }
   
  
 echo "</table>";
+}
 
 ?>
